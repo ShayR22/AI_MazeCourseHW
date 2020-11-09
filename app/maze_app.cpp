a5c429a680b38b18dc98d9c169ea1285862549df
avivlazar <avivlzr123@gmail.com>
2020-11-09 20:55:45 +0200

    Graph: add convertion and display
    add Graph and its node and edges.
    add the ability to create it from a
    given maze and display it.

--- app/maze_app.cpp
+++ app/maze_app.cpp
@@ -4,18 +4,20 @@
 #include "Maze.h"
 #include "BFSSolver.h"
 #include "BFSBiDirectionalSolver.h"
+#include "Graph.h"
 constexpr int MSIZE = 100;
 
 Maze* maze = nullptr;
 BFSSolver* bfs_solver = nullptr;
 BFSBiDirectionalSolver* bi_bfs_solver = nullptr;
+Graph* graph = nullptr;
 bool bfs_is_on = false;
 bool bi_bfs_is_on = false;
 
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT); // clean frame buffer
-	maze->drawMaze();
+	graph->draw();
 	glutSwapBuffers(); // show all
 }
 
@@ -34,6 +36,7 @@ void initGlobals()
 	maze = new Maze(MSIZE, MSIZE);
 	bfs_solver = new BFSSolver(*maze);
 	bi_bfs_solver = new BFSBiDirectionalSolver(*maze);
+	graph = new Graph(*maze);
 }
 
 void setSolverFalse()
@@ -47,6 +50,7 @@ void destroyGlobals()
 	delete maze;
 	delete bfs_solver;
 	delete bi_bfs_solver;
+	delete graph;
 }
 
 void restart()
@@ -79,6 +83,7 @@ void init()
 	srand((unsigned int)time(0));
 
 	restart();
+
 	OpenGL::width = MSIZE;
 	OpenGL::height = MSIZE;
 	OpenGL::circleR = 1 / (float)MSIZE;
@@ -90,8 +95,8 @@ int main(int argc, char* argv[])
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-	glutInitWindowSize(600, 600);
-	glutInitWindowPosition(200, 0);
+	glutInitWindowSize(1000, 1000);
+	glutInitWindowPosition(400, 0);
 	glutCreateWindow("First Example");
 
 	glutDisplayFunc(display);
@@ -106,5 +111,6 @@ int main(int argc, char* argv[])
 	init();
 	glutMainLoop();
 	destroyGlobals();
+
 	return 0;
 }
