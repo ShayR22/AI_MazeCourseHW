9f7cd840da3509fbb55ef72d3f3791dfd57c8761
ShayR <shayrashinsky@gmail.com>
2020-11-29 21:09:22 +0200

    Manager: add manager class
    add manager class to manage different types of drawable (maze
    / graph) and the types of solver (BFS / BFSBI).
    
    this would remove the logic from the main into a class
    to manage it

--- app/maze_app.cpp
+++ app/maze_app.cpp
@@ -1,77 +1,43 @@
 #include <time.h>
 #include "glut.h"
 #include "OpenGL.h"
-#include "Maze.h"
-#include "BFSSolver.h"
-#include "BFSBiDirectionalSolver.h"
-#include "Graph.h"
+#include "Manager.h"
 constexpr int MSIZE = 100;
 
-Maze* maze = nullptr;
-BFSSolver* bfs_solver = nullptr;
-BFSBiDirectionalSolver* bi_bfs_solver = nullptr;
-Graph* graph = nullptr;
-bool bfs_is_on = false;
-bool bi_bfs_is_on = false;
+Manager manager(MSIZE);
 
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT); // clean frame buffer
-	graph->draw();
+	manager.draw();
 	glutSwapBuffers(); // show all
 }
 
 void idle()
 {
-	if (bfs_is_on)
-		bfs_solver->solveIteration();
-	else if (bi_bfs_is_on)
-		bi_bfs_solver->solveIteration();
-
+	manager.solveIteration();
 	glutPostRedisplay(); // indirect call to display
 }
 
-void initGlobals()
-{
-	maze = new Maze(MSIZE, MSIZE);
-	bfs_solver = new BFSSolver(*maze);
-	bi_bfs_solver = new BFSBiDirectionalSolver(*maze);
-	graph = new Graph(*maze);
-}
-
-void setSolverFalse()
-{
-	bfs_is_on = false;
-	bi_bfs_is_on = false;
-}
-
-void destroyGlobals()
-{
-	delete maze;
-	delete bfs_solver;
-	delete bi_bfs_solver;
-	delete graph;
-}
-
-void restart()
-{
-	setSolverFalse();
-	destroyGlobals();
-	initGlobals();
-}
 
 void menu(int choice)
 {
 	switch (choice)
 	{
 	case 0: // Restart
-		restart();
+		manager.restart();
 		break;
-	case 1: // BFS
-		bfs_is_on = true;
+	case 1: // Graph
+		manager.setDrawableSolvable(DrawableType::GRAPH, SolveableType::NONE);
 		break;
-	case 2: // BFS Bi
-		bi_bfs_is_on = true;
+	case 2: // Maze
+		manager.setDrawableSolvable(DrawableType::MAZE, SolveableType::NONE);
+		break;
+	case 3: // BFS
+		manager.setDrawableSolvable(DrawableType::MAZE, SolveableType::MAZE_BFS);
+		break;
+	case 4: // BFS BI
+		manager.setDrawableSolvable(DrawableType::MAZE, SolveableType::MAZE_BFS_BI);
 		break;
 	}
 }
@@ -82,7 +48,7 @@ void init()
 	glOrtho(-1, 1, -1, 1, 1, -1);
 	srand((unsigned int)time(0));
 
-	restart();
+	manager.restart();
 
 	OpenGL::width = MSIZE;
 	OpenGL::height = MSIZE;
@@ -104,13 +70,14 @@ int main(int argc, char* argv[])
 	// menu
 	glutCreateMenu(menu);
 	glutAddMenuEntry("Restart", 0);
-	glutAddMenuEntry("BFS", 1);
-	glutAddMenuEntry("BFS BI", 2);
+	glutAddMenuEntry("Graph", 1);
+	glutAddMenuEntry("Maze", 2);
+	glutAddMenuEntry("MAZE_BFS", 3);
+	glutAddMenuEntry("MAZE_BFS BI", 4);
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
 	init();
 	glutMainLoop();
-	destroyGlobals();
 
 	return 0;
 }
